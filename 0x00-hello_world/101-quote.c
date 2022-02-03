0891c6b86aae100fc6931ef8d0a852b541c5e12b
trenfranco <trenfranco@gmail.com>
2022-02-03 11:01:57 -0800

    last 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+
+/**
+ * main - UUnix
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", 59);
+return (1);
+}
+
