5f81a56a25fa28ad7f8c91c3f11b6e161df711f6
trenfranco <trenfranco@gmail.com>
2022-02-03 08:35:06 -0800

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char a[] = "\"Programming is like building a multilingual puzzle";
+
+puts(a);
+
+return (0);
+}
+
