8464dc03c332d014d95f7869e647b7667ed2a4df
trenfranco <trenfranco@gmail.com>
2022-02-04 09:48:01 -0800

    ex 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - BAMOS
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char f;
+for (f = 'z' ; f >= 'a' ; f--)
+{
+putchar(f);
+}
+putchar('\n');
+return (0);
+}
