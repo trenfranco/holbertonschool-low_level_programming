986df69f9c35b194026f00d9c88d6ac4ab3358f6
trenfranco <trenfranco@gmail.com>
2022-02-04 08:01:46 -0800

    ej 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char nashe;
+for (nashe = 'A' ; nashe <= 'Z' ; nashe++)
+{
+putchar(nashe);
+}
+return (0);
+}
