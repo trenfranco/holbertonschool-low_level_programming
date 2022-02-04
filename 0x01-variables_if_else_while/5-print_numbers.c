09fa65b74a7693d2c962836011f9e4d94b1bd80f
trenfranco <trenfranco@gmail.com>
2022-02-04 09:22:06 -0800

    ex 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
