0b7d56f3fa69f8b56501292cb2fb8c96aae49d50
trenfranco <trenfranco@gmail.com>
2022-02-07 05:38:24 -0800

    ex 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main  entro point
+ *
+ */
+int main(void)
+{
+int i;
+char ch[] = "_putchar";
+for (i = 0 ; i <= 7 ; i++)
+{
+_putchar (ch[i]);
+}
+_putchar ('\n');
+return (0);
+}
