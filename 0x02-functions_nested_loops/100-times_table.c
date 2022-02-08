8dc5f8d8a941d8de246c1cb67e1794347e3eccc5
trenfranco <trenfranco@gmail.com>
2022-02-07 17:49:52 -0800

    k

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_times_table - dasd
+ * @n: das
+ */
+void print_times_table(int n)
+{
+int i;
+int x;
+int s;
+if (n < 1)
+n = -n;
+for (i = 0; i <= n; i++)
+{
+for (x = 0; x <= n; x++)
+{
+s = i * x;
+if (s > 9)
+_putchar(s / 10 + '0');
+_putchar(s % 10 + '0');
+if (x != n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+}
+}
+}
+}
