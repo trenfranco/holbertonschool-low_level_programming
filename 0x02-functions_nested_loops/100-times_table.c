cd683872d953e451f9b86af93870f9c9ffea5c76
trenfranco <trenfranco@gmail.com>
2022-02-07 18:02:28 -0800

    exe 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 int i;
 int x;
 int s;
-if (0 < n)
+if (0 <= n)
 {
 for (i = 0; i <= n; i++)
 {
@@ -17,13 +17,11 @@ for (x = 0; x <= n; x++)
 s = i * x;
 if (s > 999)
 {
-_putchar(s / 10 + '0');
-s = s / 10;
+_putchar(s / 1000 + '0');
 }
 if (s > 99)
 {
-_putchar(s / 10 + '0');
-s = s / 10;
+_putchar(s / 100 + '0');
 }
 if (s > 9)
 _putchar(s / 10 + '0');
