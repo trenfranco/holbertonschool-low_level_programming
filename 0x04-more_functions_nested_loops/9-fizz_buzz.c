c5c14f3b55e5bfa8f0e589715c0f670b142437a4
trenfranco <trenfranco@gmail.com>
2022-02-09 12:14:39 -0800

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,26 @@ for (a = 1 ; a <= 100 ; a++)
 {
 if (a % 3 == 0 && a % 5 == 0)
 {
-printf ("FizzBuzz ");
+printf("FizzBuzz ");
 }
 else if (a % 3 == 0)
 {
-printf ("Fizz ");
+printf("Fizz ");
 }
 else if (a % 5 == 0)
 {
-printf ("Buzz");
+printf("Buzz");
 if (a != 100)
 {
-printf (' ');
+printf(" ");
 }
 }
 else
 {
-printf ("%d ", a);
+printf("%d ", a);
 }
 }
-putchar ('\n');
+putchar('\n');
 
 return (0);
 }
