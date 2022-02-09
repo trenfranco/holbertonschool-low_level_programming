af98951837c6621d7df5e4d9590d75929fa52a89
trenfranco <trenfranco@gmail.com>
2022-02-09 11:12:04 -0800

    ex 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,21 +7,22 @@
  */
 void print_diagonal(int n)
 {
-	int a;
-	int b;
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	for (a = 0 ; a < n ; a++)
-	{
-		for (b = a ; b > 0 ; b--)
-		{
-			_putchar (' ');
-		}
-		_putchar('\\');
-		_putchar('\n');	
-	}
+int a;
+int b;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+for (a = 0 ; a < n ; a++)
+{
+for (b = a ; b > 0 ; b--)
+{
+_putchar (' ');
+}
+_putchar('\\');
+_putchar('\n');	
+}
 	
 }
 
