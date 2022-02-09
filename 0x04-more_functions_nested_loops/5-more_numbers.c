55d5cd1a5c6d2945197e60e56a7cd6df2db75411
trenfranco <trenfranco@gmail.com>
2022-02-09 08:21:32 -0800

    ex 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+
+for (a = 0 ; a <= 9 ; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
+{
+_putchar (b / 10 + '0');
+}
+_putchar (b % 10 + '0');
+}
+_putchar ('\n');
+}
+
+}
