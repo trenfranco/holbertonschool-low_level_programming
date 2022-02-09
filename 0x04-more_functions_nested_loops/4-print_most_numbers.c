b46e613f07594fa67cc3848fe2afdb0cae50d503
trenfranco <trenfranco@gmail.com>
2022-02-09 07:34:18 -0800

    ex 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+if (i != '2' && i != '4')
+{
+_putchar (i % 10 + '0');
+}
+}
+_putchar ('\n');
+}
