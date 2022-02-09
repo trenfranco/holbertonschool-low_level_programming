3fc65bd339a3905b949f1c146c7716b9e991c3c5
trenfranco <trenfranco@gmail.com>
2022-02-09 06:36:10 -0800

    ex0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ * @c: char
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
