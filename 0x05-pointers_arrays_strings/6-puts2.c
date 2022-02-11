3e0a01830b0d20ecdba6035cce3376b40b7fe059
trenfranco <trenfranco@gmail.com>
2022-02-11 13:18:33 -0800

    ex 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * puts2 - check the code
+ * @str: string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+	{
+	int i;
+	int j = _strlen(str) - 1;
+
+	for (i = 0 ; i <= j ; i++)
+		{
+		if (i % 2 == 0)
+		{
+
+		_putchar(i + '0');
+		}
+		}
+	_putchar('\n');
+
+	}
+
+
+
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int count;
+char c;
+
+c = *s;
+count = 0;
+
+while (c != '\0')
+{
+count++;
+c = *(s + count);
+
+}
+return (count);
+}
