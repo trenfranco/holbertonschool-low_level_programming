e6bf07139e7b3184b7e401de0c0b6a6f621c89f2
trenfranco <trenfranco@gmail.com>
2022-02-11 08:33:02 -0800

    ex 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @a: int a
+ * @b: int b
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+
+}
