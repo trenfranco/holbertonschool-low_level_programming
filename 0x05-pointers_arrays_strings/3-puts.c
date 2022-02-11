ad7b44a164c5f2853087db0cb0f480470fef5b72
trenfranco <trenfranco@gmail.com>
2022-02-11 10:22:22 -0800

    ex3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	char a = *str;
+	
+	while (a != '\0')
+	{
+		a = str[i];
+		_putchar(a);
+		i++;
+	}
+
+	_putchar('\n');
+
+
+
+}
