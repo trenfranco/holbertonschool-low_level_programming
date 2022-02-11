6c8ae66acb0042617cadd5fb099683d522ba544b
trenfranco <trenfranco@gmail.com>
2022-02-11 10:48:12 -0800

    ex fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,23 +2,42 @@
 
 /**
  * _puts - check the code
- *
+ * @str: string
  * Return: Always 0.
  */
 void _puts(char *str)
 {
 	int i = 0;
-	char a = *str;
+	char a = ' ';
+	int x = _strlen(str) - 1;
 
-	while (a != '\0')
+	while (i <= x)
 	{
 		a = str[i];
 		_putchar(a);
 		i++;
 	}
-
 	_putchar('\n');
-
-
-
 }
+
+/**
+ * _strlen - check the code
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+	{
+	int count;
+	char c;
+
+	c = *s;
+	count = 0;
+
+	while (c != '\0')
+	{
+	count++;
+	c = *(s + count);
+
+	}
+	return (count);
+	}
