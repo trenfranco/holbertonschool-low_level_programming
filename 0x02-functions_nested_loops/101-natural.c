8ae6455a1670d6e849ff5496284db64d04012b69
trenfranco <trenfranco@gmail.com>
2022-02-12 16:36:05 -0800

    ex 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @i: number
+ * @a: num
+ * @b: num
+ * @c: int
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int a = 0;
+int b = 0;
+int c = 0;
+
+for (i = 1024 ; i > 0 ; i--)
+	{
+	if (i % 3 != 1 && i != 1024)
+	{
+	a = a + i;
+	}
+	}
+
+for (i = 1024 ; i > 0 ; i--)
+	{
+	if (i % 5 != 1 && i != 1024)
+	{
+	b = b + i;
+	}
+	}
+c = b + a;
+printf("%i", c);
+printf("\n");
+
+
+
+}
