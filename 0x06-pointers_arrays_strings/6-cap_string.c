244775d32925175e6a8e4217bbc71e6ce6d5eaf1
trenfranco <trenfranco@gmail.com>
2022-02-15 12:27:15 -0800

    ex 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ *@a: char
+ * Return: Always 0.
+ */
+char *cap_string(char *a)
+{
+int i;
+
+while (a[i] != '\0')
+	{
+	if (a[i] == ' ' || a[i] == '	' || a[i] == '\n'
+|| a[i] == ',' || a[i] == ';' || a[i] == '.' ||
+a[i] == '!' || a[i] == '?' || a[i] == '"' ||
+a[i] == '(' || a[i] == ')' || a[i] == '{' || a[i] == '}')
+	{
+	if (a[(i + 1)] >= 'a' && a[(i + 1)] <= 'z')
+	{
+	a[(i + 1)] = a[(i + 1)] - 32;
+	}
+	}
+	i++;
+	}
+	return (a);
+
+}
