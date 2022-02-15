56bf80d634e17b5ec7ab36221061fdcc148a12cc
trenfranco <trenfranco@gmail.com>
2022-02-15 10:05:06 -0800

    ex 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * _strcmp - check the code
+ *@s1: stringaa 1
+ *@s2: str 2
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+if (_strlen(s1) < _strlen(s2))
+	{
+	return (-15);
+	}
+else if (_strlen(s1) > _strlen(s2))
+	{
+	return (15);
+	}
+else
+{
+return (0);
+}
+
+
+}
+
+/**
+ * _strlen - retorna el largo
+ * @s: char
+ * Return: largo
+ */
+int _strlen(char *s)
+{
+int cont = 0;
+char l = '0';
+if (*s == '\0')
+return (0);
+while (l != '\0')
+{
+cont++;
+l = *(s + cont);
+}
+return (cont);
+}
