a8cc19c248b7fb47d92ef0852f84d08a809065ee
trenfranco <trenfranco@gmail.com>
2022-02-16 07:32:40 -0800

    ex 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - check the code for
+ *@a: char
+ * Return: Always 0.
+ */
+char *leet(char *a)
+{
+int i = 0;
+int b = 0;
+char j[11] = {"aAeEoOtTlL"};
+char n[11] = {"4433007711"};
+
+while(a[i] != '\0')
+	{
+	for (b = 0 ; b < 11 ; b++)
+		{
+		if (a[i] == j[b])
+			{
+			a[i] = n[b];
+			}
+		}
+	i++;
+	}
+return(a);
+
+}
