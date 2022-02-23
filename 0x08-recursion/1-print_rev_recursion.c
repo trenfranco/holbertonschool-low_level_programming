27ca74e3fd0080f6ece64fdf96957e25644b02c6
trenfranco <trenfranco@gmail.com>
2022-02-23 07:35:54 -0800

    ex 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ *@s: str
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+	{
+	return;
+	}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
