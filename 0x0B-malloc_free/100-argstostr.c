244d415068fdc92beed011bde78349dbd6bf80d3
trenfranco <trenfranco@gmail.com>
2022-03-02 12:53:17 -0800

    ex 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * argstostr - concatena argumentos
+ * @ac: largo de argumentos
+ * @av: args
+ * Return: puntero a cadenas concatenadas
+ */
+char *argstostr(int ac, char **av)
+{
+char *p;
+int b, i, c, cont = 0;
+
+if (av == NULL || ac == 0)
+	return (NULL);
+
+cont = 0;
+for (i = 0; i < ac; i++)
+{
+b = 0;
+	while (av[i][b] != '\0')
+	{
+	b++;
+	cont++;
+	}
+cont++;
+}
+
+cont++;
+p = malloc(sizeof(char) * cont);
+
+b = 0;
+c = 0;
+for (i = 0; i < ac; i++)
+{
+	b = 0;
+	while (av[i][b] != '\0')
+	{
+	p[c] = av[i][b];
+	c++;
+	b++;
+	}
+	p[c] = '\n';
+	c++;
+}
+p[c] = '\0';
+return (p);
+}
