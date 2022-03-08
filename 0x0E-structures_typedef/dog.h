2e428f40840335d9ce6625308cd7d198f6f89d69
trenfranco <trenfranco@gmail.com>
2022-03-08 07:58:31 -0800

    ex 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
