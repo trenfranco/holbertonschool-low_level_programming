3f79def6f9dcaf65b54cefe544225bec5d5eaeeb
trenfranco <trenfranco@gmail.com>
2022-03-08 10:01:18 -0800

    ex 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 		}
 		else
 			{
@@ -24,7 +24,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 		{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 		}
 		else
 			{
