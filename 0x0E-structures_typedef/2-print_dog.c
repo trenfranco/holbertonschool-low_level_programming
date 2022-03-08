8b35be105627248dbda4d58b02ebd5cd32609304
trenfranco <trenfranco@gmail.com>
2022-03-08 09:51:08 -0800

    ex 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog
+ * @d: dog
+ *
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		{
+		printf("Name: (nil)");
+		}
+		else
+			{
+			printf("Name: %s\n", d->name);
+			}
+
+			printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		{
+		printf("Owner: (nil)");
+		}
+		else
+			{
+			printf("Owner: %s\n", d->owner);
+			}
+
+}
