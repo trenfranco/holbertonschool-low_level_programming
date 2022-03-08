79cc12a41071026d5afffcd33a40d39dc3344615
trenfranco <trenfranco@gmail.com>
2022-03-08 08:38:08 -0800

    ex 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initalazing struct dog
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+return;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+
 #ifndef DOG_H
 #define DOG_H
 /**
@@ -13,4 +14,6 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
