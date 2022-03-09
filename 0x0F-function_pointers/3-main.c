88a0b0b3698671b4b517a249069711930eb622a7
trenfranco <trenfranco@gmail.com>
2022-03-09 13:08:04 -0800

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,10 +17,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][1] != '\0') && (argv[2][0] != '+' ||
+	if (f == NULL || argv[2][1] != '\0')
+	{
+/**
+		if ((argv[2][1] != '\0') && (argv[2][0] != '+' ||
 		argv[2][0] != '-' || argv[2][0] != '*' ||
 		argv[2][0] != '/' || argv[2][0] != '%'))
-	{
+**/	
 		printf("Error\n");
 		exit(99);
 	}
