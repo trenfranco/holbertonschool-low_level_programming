ec7007c6c59980250e486e301a37bd32d5c7e9d7
trenfranco <trenfranco@gmail.com>
2022-03-09 13:12:37 -0800

    ex 3 fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((argv[2][1] != '\0') && (argv[2][0] != '+' ||
+	if (((argv[2][1] != '\0') && (argv[2][0] != '+' ||
 		argv[2][0] != '-' || argv[2][0] != '*' ||
-		argv[2][0] != '/' || argv[2][0] != '%'))
+		argv[2][0] != '/' || argv[2][0] != '%')) || argv[2][0] == '\\')
 	{
 
 		printf("Error\n");
