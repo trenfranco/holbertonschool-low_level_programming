848cfaae300d06d4a61b8f5f577e02e71db2d296
trenfranco <trenfranco@gmail.com>
2022-03-17 16:53:15 -0700

    ex 04

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list -  frees a list
+ * @head: head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *last;
+
+	if (head == NULL)
+		free(head);
+	if (head->next == NULL)
+	{
+		free(head->str);
+		free(head);
+		return;
+	}
+	last = head;
+	while (last->next != NULL)
+	{
+		free(last->str);
+		free(last);
+		last = last->next;
+	}
+free(last->str);
+free(last);
+return;
+
+}
