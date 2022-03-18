42545cf143eb753ef7ed3483bdc0f4d86307623a
trenfranco <trenfranco@gmail.com>
2022-03-17 17:12:07 -0700

    ex 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * FrancoStart -  start before main func
+ *
+ * Return: void
+ */
+void FrancoStart(void) __attribute__ ((constructor));
+
+void FrancoStart(void)
+{
+	printf("You're beat! and yet,
+			you must allow,\nI bore my house upon my back!\n");
+}
