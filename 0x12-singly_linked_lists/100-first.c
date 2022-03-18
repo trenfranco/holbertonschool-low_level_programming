66c797d214bd2d0418c1a55895c46e5e0f97db29
trenfranco <trenfranco@gmail.com>
2022-03-17 17:17:25 -0700

    100 fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void FrancoStart(void) __attribute__ ((constructor));
 
 void FrancoStart(void)
 {
-	printf("You're beat! and yet,
-			you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
