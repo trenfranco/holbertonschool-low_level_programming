a65d1be4a7d24f8a20d3546f15091e0a3e9daf81
trenfranco <trenfranco@gmail.com>
2022-03-21 12:07:15 -0700

    ex 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - insert onde at index
+ * @head: head
+ * @idx: index
+ * @n: n
+ * Return: addressof new node or null
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *aux, *new, *pedro;
+	unsigned int i;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	aux = *head;
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (aux == NULL)
+			return (NULL);
+		aux = aux->next;/*nos paramos en el nodo q queremos*/
+	}
+	pedro = aux->next;
+	aux->next = new;
+	new->next = pedro;
+	return (new);
+}
