f82a2108fbdf6d5933a6676e0862a7602e5dee8c
trenfranco <trenfranco@gmail.com>
2022-03-21 09:53:40 -0700

    ex 05

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - prints all the elements of a listint_t
+ * @head: head
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *franco;
+
+	while (*head != NULL)
+	{
+		franco = *head;
+		*head = (*head)->next;
+		free(franco);
+		franco = NULL;
+	}
+
+}
