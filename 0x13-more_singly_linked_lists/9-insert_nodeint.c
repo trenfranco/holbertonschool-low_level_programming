9141debad3ffbccc6809a875ac037a18bad430cd
trenfranco <trenfranco@gmail.com>
2022-03-22 13:03:22 -0700

    ex 9fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (aux == NULL)
 			return (NULL);
 	}
-	aux->next = aux->next;
+	new->next = aux->next;
 	aux->next = new;
 	return (new);
 }
