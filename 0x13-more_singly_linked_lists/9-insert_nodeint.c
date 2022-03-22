17b8f480dcd434ff907765c800431cfc0caa7ad5
trenfranco <trenfranco@gmail.com>
2022-03-22 12:28:19 -0700

    ex 09 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *aux, *new, *pedro;
 	unsigned int i;
 
+	if (*head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
