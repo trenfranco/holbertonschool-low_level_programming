5921f742b7aa6ab9ee14537f5b3493c16da3d326
trenfranco <trenfranco@gmail.com>
2022-03-22 13:00:04 -0700

    ex 9fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,26 +10,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *aux, *new, *pedro;
+	listint_t *aux, *new;
 	unsigned int i;
 
-	if (head == NULL && idx != 0)
-		return (NULL);
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 
 	aux = *head;
-
-	for (i = 0; i < (idx - 1); i++)
+	if (idx == 0)
 	{
+		new->next = *head;
+		(*head) = new;
+		return (new);
+	}
+
+	for (i = 0; i < (idx - 1) && aux; i++)
+	{
+		aux = aux->next;
 		if (aux == NULL)
 			return (NULL);
-		aux = aux->next;/*nos paramos en el nodo q queremos*/
 	}
-	pedro = aux->next;
+	aux->next = aux->next;
 	aux->next = new;
-	new->next = pedro;
 	return (new);
 }
