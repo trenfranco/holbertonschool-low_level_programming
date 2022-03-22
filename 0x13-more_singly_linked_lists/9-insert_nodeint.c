bc4efd66e0d8ae915b7c3dc48b1edae1dc2c9a21
trenfranco <trenfranco@gmail.com>
2022-03-22 13:01:56 -0700

    ex 9fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (i = 0; i < (idx - 1) && aux; i++)
+	for (i = 1; i < idx && aux; i++)
 	{
 		aux = aux->next;
 		if (aux == NULL)
