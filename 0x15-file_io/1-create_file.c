aff62724371b034b77bfbb60e2df37e0214f8e9c
trenfranco <trenfranco@gmail.com>
2022-03-28 13:00:31 -0700

    ex 01

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_file - creates a file.
+ * @filename: pointer to name file
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, written, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IWOTH | S_IROTH);
+
+	if (fd == -1)
+		return (-1);
+	i = 0;
+	while (text_content[i] != '\0')
+		i++;
+
+	written = write(fd, text_content, i);
+
+	close(fd);
+
+	if (written == -1)
+		return (-1);
+	return (1);
+}
