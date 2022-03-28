318b4c027b6669164d84cf757049297ba004bb2c
trenfranco <trenfranco@gmail.com>
2022-03-28 13:13:12 -0700

    ex  1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWOTH | S_IROTH);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
