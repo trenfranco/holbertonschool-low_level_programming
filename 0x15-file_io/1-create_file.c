e717fa4ef458999222efef86266712eb640f4c5e
trenfranco <trenfranco@gmail.com>
2022-03-28 13:05:09 -0700

    ex  1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,11 @@ int create_file(const char *filename, char *text_content)
 
 	close(fd);
 
+
 	if (written == -1)
 		return (-1);
-	return (1);
+
+	if (written == i)
+		return (1);
+	return (-1);
 }
