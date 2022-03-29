6265cfe584c61a392fdb79657073b6fe6e612af0
trenfranco <trenfranco@gmail.com>
2022-03-28 17:14:03 -0700

    ex 03 fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,20 +16,23 @@ int main(int argc, char **argv)
 	char *file_from, *file_to, *buffer;
 
 	buffer = malloc(1024);
+	if (!buffer)
+		return (-1);
 
 	file_from = argv[1];
 	file_to = argv[2];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdt = open(file_to, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fdt = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR
+		| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
 	if (fdt == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	fdf = open(file_from, O_RDONLY);
@@ -37,29 +40,31 @@ int main(int argc, char **argv)
 	if (fdf == -1)
 	{
 		funcclose(fdt);
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	sizef = 1024;
-	for(; sizef == 1024;)
+	for (; sizef == 1024;)
 	{
-	 	sizef = read(fdf, buffer, 1024);
+		sizef = read(fdf, buffer, 1024);
 
 		if (sizef == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
+			funcclose(fdf);
+			funcclose(fdt);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
 
-		if (write (fdt, buffer, sizef) == -1)
+		if (write(fdt, buffer, sizef) == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
 	funcclose(fdf);
 	funcclose(fdt);
-	return(0);
+	return (0);
 }
 /**
  * funcclose - close and if-1 prints err
@@ -70,7 +75,7 @@ void funcclose(int a)
 {
 	if (close(a) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %i\n", a);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", a);
 		exit(100);
 	}
 
