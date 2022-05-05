a7dd079e6e4d8f05d27acab733be415f05051349
trenfranco <trenfranco@gmail.com>
2022-05-05 13:26:34 -0700

    ex 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *cute;
 
+	if (head == NULL)
+		return (NULL);
+
 	cute = malloc(sizeof(dlistint_t));
 	if (cute == NULL)
 		return (NULL);
