f7e1315c103af2ff5a58056598dbd4c7f4e425e0
trenfranco <trenfranco@gmail.com>
2022-05-05 15:49:21 -0700

    ex 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,6 +18,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (i < index && temp->next != NULL)
 	{
+		if (index == 0)
+			return (head);
 		temp = temp->next;
 		i++;
 	}
