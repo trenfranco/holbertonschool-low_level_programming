002660c23734977f889831a78c148108ded3cdc2
trenfranco <trenfranco@gmail.com>
2022-05-26 13:57:43 -0700

    betty fixed

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of the table
+ * Return: Pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = calloc(sizeof(hash_node_t), size);
+
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
