5dcd72df1cb9855cfcd385e693882c35483787e3
trenfranco <trenfranco@gmail.com>
2022-07-14 19:37:01 -0700

    ex 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,11 +9,11 @@ def island_perimeter(grid):
     for f in range(len(grid)):
         for c in range(len(grid[0])):
             if grid[f][c] == 1:
-                if c == len(grid) - 1 or grid[f][c + 1] == 0:
+                if c == len(grid[0]) - 1 or grid[f][c + 1] == 0:
                     per = per + 1
                 if c == 0 or grid[f][c - 1] == 0:
                     per = per + 1
-                if f == len(grid[0]) or grid[f + 1][c] == 0:
+                if f == len(grid) - 1 or grid[f + 1][c] == 0:
                     per = per + 1
                 if f == 0 or grid[f - 1][c] == 0:
                     per = per + 1
