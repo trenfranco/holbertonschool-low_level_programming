7c7cd04cc1a33898a77636b2f67ecc8f175e425c
trenfranco <trenfranco@gmail.com>
2022-08-26 09:37:46 -0700

    ex 0 README

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 	int val = -1;
 
-	if (!array)
+	if (!array || !size || !value || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
